ccdabe56c1140589c34b64046e33c81129f81cdb
Keshav Koirala <keshavkoirala933@gmail.com>
2025-03-16 11:16:59 +0545

    Updated feature/fixed bugs

--- app.js
+++ app.js
@@ -5,25 +5,33 @@
     let equal = document.querySelector('.btn-equal');
 
 
-    buttons.forEach(function(button){
-        button.addEventListener('click',function(e){
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
             let value = e.target.dataset.num;
-            screen.value += value;
-        })
+            if (value !== undefined) {  // Prevents "undefined" from being appended
+                screen.value += value;
+            }
+        });
     });
+    
 
-    equal.addEventListener('click', function(e){
-        if(screen.value === ''){
-           screen.value = "Please enter"; 
-        }else{
-            let answer = eval(screen.value);
-            screen.value = answer;
+    equal.addEventListener('click', function(e) {
+        if (screen.value === "" || screen.value.includes("undefined")) {
+            screen.value = "Error";  // Prevent invalid evaluation
+        } else { 
+            try {
+                let answer = eval(screen.value);
+                screen.value = answer;
+                
+            } catch (error) {
+                screen.value = "Error"; // Handle syntax errors
+            }
         }
-    })
-
+    });
+    
     clear.addEventListener('click', function(e)
     {
-        screenvalue = ''; 
+        screen.value = ''; 
     })
 
 })();
